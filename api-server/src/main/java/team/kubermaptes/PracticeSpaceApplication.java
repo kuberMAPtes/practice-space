1ef90d503d76e6c26462712d9919e03330adece4
pgd <rudeh1253@gmail.com>
2024-05-14 17:50:34 +0900

    chore: initialize api server project

---
+++ api-server/src/main/java/team/kubermaptes/PracticeSpaceApplication.java
@@ -0,0 +1,13 @@
+package team.kubermaptes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticeSpaceApplication.class, args);
+	}
+
+}
---
+++ api-server/src/test/java/team/kubermaptes/PracticeSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package team.kubermaptes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
