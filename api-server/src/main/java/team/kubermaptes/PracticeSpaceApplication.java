877c87e1830b50aca189841f0275e3235b6b27dc
최현준 <immio0@kangwon.ac.kr>
2024-05-16 21:02:05 +0900

    24-05-16 가즈앗

--- api-server/src/main/java/team/kubermaptes/PracticeSpaceApplication.java
+++ api-server/src/main/java/team/kubermaptes/PracticeSpaceApplication.java
@@ -8,6 +8,8 @@ public class PracticeSpaceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PracticeSpaceApplication.class, args);
+
+		System.out.println("Hello World");
 	}
 
 }
