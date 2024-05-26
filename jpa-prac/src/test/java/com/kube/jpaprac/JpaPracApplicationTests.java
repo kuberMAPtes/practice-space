ad82964c196d775294ec07cd46df777e68dfa068
pgd <rudeh1253@gmail.com>
2024-05-26 18:20:25 +0900

    Initialize JPA 연습용 프로젝트

---
+++ jpa-prac/src/test/java/com/kube/jpaprac/JpaPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kube.jpaprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
