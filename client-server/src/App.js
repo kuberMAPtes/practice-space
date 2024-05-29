1776e64a6a410f5b0bd647a22133f39613ec8f39
pgd <rudeh1253@gmail.com>
2024-05-30 08:40:09 +0900

    리액트 App.js 롤백
    성진님 거하고 겹칠까봐

--- client-server/src/App.js
+++ client-server/src/App.js
@@ -1,15 +1,24 @@
-import logo from "./logo.svg";
-import "./App.css";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Maps from "./component/page/Maps";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/map" element={<Maps />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
